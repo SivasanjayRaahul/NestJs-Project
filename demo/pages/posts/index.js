8b4110f6bf93dcaad96ee8b3b870f51b67b7146f
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2023-01-07 23:24:25 +0530

    [Sivasanjay] Authenticate posts page

--- demo/pages/posts/index.js
+++ demo/pages/posts/index.js
@@ -3,8 +3,25 @@
 import React from "react";
 import PostsCount from "@/components/PostsCount";
 import DisplayPosts from "@/components/DisplayPosts";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { signIn } from "next-auth/react";
 
 export default function ViewList({ posts }) {
+  useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn()
+    }
+  });
+
+  // // useEffect(() => {
+  // //   if (status === 'unauthenticated') signIn();
+  // // }, [status]);
+
+  // if (status !== 'authenticated') {
+  //   return <h2>Loading...</h2>;
+  // }
   return (
     <>
       <h1>Posts</h1>
