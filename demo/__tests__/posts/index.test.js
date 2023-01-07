04eff5b5deca0d22f85401f2079a92377e14e105
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2023-01-07 16:26:24 +0530

    [Sivasanjay] Add test for post count in view pages

--- demo/__tests__/posts/index.test.js
+++ demo/__tests__/posts/index.test.js
@@ -1,13 +1,33 @@
-import { render, screen } from "@testing-library/react"
+/** @format */
 
-import Posts from "@/pages/posts/index"
+import { render } from "@testing-library/react";
+import Posts from "@/pages/posts/index";
+
+export const mockedPosts = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 2,
+    id: 2,
+    title: "aut autem",
+    completed: true,
+  },
+];
 
 describe("render view posts page", () => {
+  it("display posts at top", () => {
+    const { getByText } = render(<Posts posts={mockedPosts} />);
 
-    it("display posts at top", () => {
-        const { getByText}=render(<Posts />)
-        
-        expect(getByText("Posts")).toBeInTheDocument();
+    expect(getByText("Posts")).toBeInTheDocument();
+  });
 
-    })
-})
+  it("display no of posts as number", () => {
+    const { getByText } = render(<Posts posts={mockedPosts} />);
+
+    expect(getByText("Total posts: 2")).toBeInTheDocument;
+  });
+});
