b342284cca936faba51c1dc34518ddaaac8389e6
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2023-01-07 13:20:07 +0530

    [Sivasanjay] Add link to buttons in home page

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -2,16 +2,22 @@
 
 
 import styles from "@/pages/index.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.h1}>POSTS</h1>
 
-      <button className={styles.createButton}>Create</button>
-      <button className={styles.viewButton}>View</button>
-      <button className={styles.updateButton}>Update</button>
-
+      <button className={styles.createButton}>
+        <Link href="/posts/create">Create</Link>
+      </button>
+      <button className={styles.viewButton}>
+        <Link href="/posts">View</Link>
+      </button>
+      <button className={styles.updateButton}>
+        <Link href="/posts">Update</Link>
+      </button>
     </>
   );
 }
---
+++ demo/pages/posts/index.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from "react";
+
+export default function ViewList() {
+  return (
+    <>
+      <h1>ViewList</h1>
+    </>
+  );
+}
